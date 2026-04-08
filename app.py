ccc2f0fcef0e8fd2bffece848c36702f3df5f968
Challagundla Neelima <challagundlaneelima12@gmail.com>
2026-04-08 23:23:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,7 @@ from inference import reset
 def test():
     return reset()
 
-gr.Interface(fn=test, inputs=[], outputs="text").launch(server_name="0.0.0.0", server_port=7860)
+gr.Interface(fn=test, inputs=[], outputs="text").launch(
+    server_name="0.0.0.0",
+    server_port=7860
+)
